f441152e1043fbcac6f707eab57a239aeeeb1c9e
Brandon <esolutions0510@gmail.com>
2025-04-11 14:43:19 -0500

    added railway.json

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from telegram import Update, ChatPermissions
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from datetime import timedelta
+import re
 
 load_dotenv()  # Load .env vars
 
@@ -12,6 +13,7 @@ BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 # File paths for phrases
 BAN_PHRASES_FILE = "ban_phrases.txt"
 MUTE_PHRASES_FILE = "mute_phrases.txt"
+DELETE_PHRASES = "delete_phrases.txt"
 
 # Mute duration in seconds (3 days)
 MUTE_DURATION = 3 * 24 * 60 * 60
@@ -23,6 +25,7 @@ def load_phrases(file_path):
 # Load phrases from files
 BAN_PHRASES = load_phrases(BAN_PHRASES_FILE)
 MUTE_PHRASES = load_phrases(MUTE_PHRASES_FILE)
+DELETE_PHRASES = load_phrases(DELETE_PHRASES)
 
 def check_message(update: Update, context: CallbackContext):
     print(f"[DEBUG] Chat ID: {update.effective_chat.id}")
@@ -52,18 +55,27 @@ def check_message(update: Update, context: CallbackContext):
 
     # Check for ban phrases
     for phrase in BAN_PHRASES:
-        if phrase in message_text:
+        pattern = rf'\b{re.escape(phrase)}\b'
+        if re.search(pattern, message_text):
             context.bot.kick_chat_member(chat_id=chat_id, user_id=user.id)
             message.reply_text(f"arc angel fallen. {user.first_name} has been banned.")
             return
 
     # Check for mute phrases
     for phrase in MUTE_PHRASES:
-        if phrase in message_text:
+        pattern = rf'\b{re.escape(phrase)}\b'
+        if re.search(pattern, message_text):
             until_date = message.date + timedelta(seconds=MUTE_DURATION)
             permissions = ChatPermissions(can_send_messages=False)
             context.bot.restrict_chat_member(chat_id=chat_id, user_id=user.id, permissions=permissions, until_date=until_date)
             message.reply_text(f"{user.first_name} has been muted for 3 days.")
+            return
+        
+    # Check for delete phrases
+    for phrase in DELETE_PHRASES:
+        pattern = rf'\b{re.escape(phrase)}\b'
+        if re.search(pattern, message_text):
+            context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
             return
 
 def main():
