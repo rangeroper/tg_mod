1f9058fa199d2f0cc2bd5189d264adaf6732a33b
Brandon <esolutions0510@gmail.com>
2025-04-25 19:36:08 -0500

    implemented more regex patterns to handle inappropriate language - mute phrases

--- bot.py
+++ bot.py
@@ -53,53 +53,56 @@ def contains_multiplication_phrase(text):
     return re.search(pattern, text)
 
 def contains_inappropriate_language_muted(text: str) -> bool:
+    # The pattern contains a series of common offensive words and their variations, using a non-greedy match for spaces and special characters
     pattern = re.compile(
         r"""
         \b(
-            d[\W_]*u[\W_]*m[\W_]*p[\W_]*[\W_]*i[\W_]*t[\W_]*|               # dump it
-            c[\W_]*l[\W_]*[o0][\W_]*w[\W_]*n[\W_]*|                         # clown
-            h[\W_]*[o0][\W_]*r[\W_]*n[\W_]*y[\W_]*|                         # horny
-            p[\W_]*[uµv][\W_]*s[\W_]*s[\W_]*y[\W_]*|                         # pussy
-            b[\W_]*[i1!|l][\W_]*[t+7][\W_]*[c(k]{1,2}|k)[\W_]*[h]*[\W_]*|  # bitch (b1tch, b!tch, b0tch)
-            a[\W_]*[s$5]{1,2}[\W_]*|                                          # ass
-            [fph][\W_]*[uµv][\W_]*[c(k]{1,2}|q)[\W_]*[k]*[\W_]*|            # fuck/fuq
-            s[\W_]*h[\W_]*[i1!|l][\W_]*[t+7][\W_]*|                         # shit
-            d[\W_]*[i1!|l][\W_]*[c(k]{1,2}|k)[\W_]*|                        # dick
-            c[\W_]*[uµ][\W_]*[n][\W_]*[t+7][\W_]*|                          # cunt
-            t[\W_]*[i1!|l][\W_]*[t+7][\W_]*[s$5]*[\W_]*|                    # tits
-            n[\W_]*[i1!|l][\W_]*[g9q]{1,2}[\W_]*[a@4][\W_]*|                # nigga
-            n[\W_]*[i1!|l][\W_]*[g9q]{1,2}[\W_]*[e3][\W_]*r[\W_]*|          # nigger
-            w[\W_]*h[\W_]*[o0][\W_]*r[\W_]*[e3][\W_]*|                      # whore
-            s[\W_]*[l1!|i][\W_]*[uµ][\W_]*[t+7][\W_]*|                      # slut
-            j[\W_]*[i1!|l][\W_]*z[\W_]*z*[\W_]*|                            # jizz
-            c[\W_]*[o0][\W_]*[c(k]{1,2}|q)[\W_]*|                           # cock
-            b[\W_]*[o0][\W_]*[o0][\W_]*b[\W_]*s*[\W_]*|                     # boobs
-            a[\W_]*n[\W_]*[a@4][\W_]*[l1!|i][\W_]*|                         # anal
-            o[\W_]*r[\W_]*g[\W_]*[yie1][\W_]*|                              # orgy
-            s[\W_]*[e3][\W_]*[x%][\W_]*|                                    # sex
-            p[\W_]*[e3][\W_]*[n][\W_]*[i1!|l][\W_]*[s$5][\W_]*|            # penis
-            v[\W_]*[a@4][\W_]*g[\W_]*[i1!|l][\W_]*n[\W_]*[a@4][\W_]*|       # vagina
-            m[\W_]*[i1!|l][\W_]*[l1!|i][\W_]*[fph][\W_]*|                   # milf
-            d[\W_]*[i1!|l][\W_]*[l1!|i][\W_]*[d][\W_]*[o0][\W_]*|           # dildo
-            f[\W_]*[a@4][\W_]*p[\W_]*|                                      # fap
-            s[\W_]*p[\W_]*[e3][\W_]*r[\W_]*[m][\W_]*|                       # sperm
-            b[\W_]*l[\W_]*[o0][\W_]*w[\W_]*[j][\W_]*[o0][\W_]*[b][\W_]*|    # blowjob
-            s[\W_]*u[\W_]*c[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*[d][\W_]*i[\W_]*c[\W_]*|  # suck my dick variants
+            d[\W_]*u[\W_]*m[\W_]*p[\W_]*[\W_]*i[\W_]*t[\W_]*|                  # dump it
+            c[\W_]*l[\W_]*[o0][\W_]*w[\W_]*n[\W_]*|                              # clown
+            h[\W_]*[o0][\W_]*r[\W_]*n[\W_]*y[\W_]*|                              # horny
+            p[\W_]*[uµv][\W_]*s[\W_]*s[\W_]*y[\W_]*|                              # pussy
+            b[\W_]*[i1!|l][\W_]*[t+7][\W_]*[c(k]{1,2}|k)[\W_]*[h]*[\W_]*|       # bitch (b1tch, b!tch, b0tch)
+            a[\W_]*[s$5]{1,2}[\W_]*|                                               # ass
+            [fph][\W_]*[uµv][\W_]*[c(k]{1,2}|q)[\W_]*[k]*[\W_]*|                 # fuck/fuq
+            s[\W_]*h[\W_]*[i1!|l][\W_]*[t+7][\W_]*|                              # shit
+            d[\W_]*[i1!|l][\W_]*[c(k]{1,2}|k)[\W_]*|                             # dick
+            c[\W_]*[uµ][\W_]*[n][\W_]*[t+7][\W_]*|                               # cunt
+            t[\W_]*[i1!|l][\W_]*[t+7][\W_]*[s$5]*[\W_]*|                         # tits
+            n[\W_]*[i1!|l][\W_]*[g9q]{1,2}[\W_]*[a@4][\W_]*|                     # nigga
+            n[\W_]*[i1!|l][\W_]*[g9q]{1,2}[\W_]*[e3][\W_]*r[\W_]*|               # nigger
+            w[\W_]*h[\W_]*[o0][\W_]*r[\W_]*[e3][\W_]*|                           # whore
+            s[\W_]*l[\W_]*[i1!|i][\W_]*[uµ][\W_]*[t+7][\W_]*|                    # slut
+            j[\W_]*[i1!|l][\W_]*z[\W_]*z*[\W_]*|                                 # jizz
+            c[\W_]*[o0][\W_]*[c(k]{1,2}|q)[\W_]*|                                # cock
+            b[\W_]*[o0][\W_]*[o0][\W_]*b[\W_]*s*[\W_]*|                          # boobs
+            a[\W_]*n[\W_]*[a@4][\W_]*[l1!|i][\W_]*|                              # anal
+            o[\W_]*r[\W_]*g[\W_]*[yie1][\W_]*|                                   # orgy
+            s[\W_]*[e3][\W_]*[x%][\W_]*|                                         # sex
+            p[\W_]*[e3][\W_]*[n][\W_]*[i1!|l][\W_]*[s$5][\W_]*|                 # penis
+            v[\W_]*[a@4][\W_]*g[\W_]*[i1!|l][\W_]*n[\W_]*[a@4][\W_]*|            # vagina
+            m[\W_]*[i1!|l][\W_]*[l1!|i][\W_]*[fph][\W_]*|                        # milf
+            d[\W_]*[i1!|l][\W_]*[l1!|i][\W_]*[d][\W_]*[o0][\W_]*|                # dildo
+            f[\W_]*[a@4][\W_]*p[\W_]*|                                           # fap
+            s[\W_]*p[\W_]*[e3][\W_]*r[\W_]*[m][\W_]*|                            # sperm
+            b[\W_]*l[\W_]*[o0][\W_]*w[\W_]*[j][\W_]*[o0][\W_]*[b][\W_]*|         # blowjob
+            s[\W_]*u[\W_]*c[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*[d][\W_]*i[\W_]*c[\W_]*| # suck my dick variants
             s[\W_]*u[\W_]*c[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*d[\W_]*i[\W_]*k[\W_]*|  # suck my dik
-            s[\W_]*u[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*d[\W_]*![\W_]*k[\W_]*|  # suk my dik
-            s[\W_]*u[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*d[\W_]*1[\W_]*k[\W_]*|  # suk my d1ck
-            b[\W_]*l[\W_]*i[\W_]*t[\W_]*c[\W_]*h[\W_]*|                    # bitch variants
-            b[\W_]*i[\W_]*t[\W_]*[c(k]{1,2}|k)[\W_]*h[\W_]*|                # b1tch, b!tch, b0tch
-            a[\W_]*s[\W_]*s[\W_]*|                                           # ass
-            d[\W_]*i[\W_]*c[\W_]*k[\W_]*|                                    # dick
-            n[\W_]*i[\W_]*g[\W_]*g[\W_]*a[\W_]*|                            # nigga variants
-            n[\W_]*i[\W_]*g[\W_]*g[\W_]*e[\W_]*r[\W_]*|                     # nigger variants
-            b[\W_]*o[\W_]*o[\W_]*b[\W_]*s*[\W_]*|                           # boobs
-            b[\W_]*l[\W_]*o[\W_]*w[\W_]*j[\W_]*o[\W_]*b[\W_]*|              # blowjob
+            s[\W_]*u[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*d[\W_]*![\W_]*k[\W_]*| # suk my dik
+            s[\W_]*u[\W_]*k[\W_]*[\W_]*m[\W_]*y[\W_]*[\W_]*d[\W_]*1[\W_]*k[\W_]*| # suk my d1ck
+            b[\W_]*l[\W_]*i[\W_]*t[\W_]*c[\W_]*h[\W_]*|                         # bitch variants
+            b[\W_]*i[\W_]*t[\W_]*[c(k]{1,2}|k)[\W_]*h[\W_]*|                     # b1tch, b!tch, b0tch
+            a[\W_]*s[\W_]*s[\W_]*|                                                # ass
+            d[\W_]*i[\W_]*c[\W_]*k[\W_]*|                                         # dick
+            n[\W_]*i[\W_]*g[\W_]*g[\W_]*a[\W_]*|                                 # nigga variants
+            n[\W_]*i[\W_]*g[\W_]*g[\W_]*e[\W_]*r[\W_]*|                          # nigger variants
+            b[\W_]*o[\W_]*o[\W_]*b[\W_]*s*[\W_]*|                                # boobs
+            b[\W_]*l[\W_]*o[\W_]*w[\W_]*j[\W_]*o[\W_]*b[\W_]*|                   # blowjob
         )\b
         """,
         re.IGNORECASE | re.VERBOSE
     )
+    
+    # Search for a match and return a boolean value indicating if it was found
     return bool(pattern.search(text))
 
 def check_message(update: Update, context: CallbackContext):
