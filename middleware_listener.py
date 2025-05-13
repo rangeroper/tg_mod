2800eca2125caf8cbc74aa84ae8fc0d5473195a0
Brandon <esolutions0510@gmail.com>
2025-05-13 18:12:35 -0500

    bug fix

--- middleware_listener.py
+++ middleware_listener.py
@@ -7,5 +7,6 @@ load_dotenv()
 MIDDLEWARE_CHAT_ID = int(os.getenv("MIDDLEWARE_CHAT_ID"))
 
 def check_middleware_message(update, context):
+    print(f"[ANY MESSAGE] From chat ID: {update.message.chat.id}, text: {update.message.text}")
     if update.message.chat.id == MIDDLEWARE_CHAT_ID:
         print(f"[MIDDLEWARE GROUP] {update.message.text}")
